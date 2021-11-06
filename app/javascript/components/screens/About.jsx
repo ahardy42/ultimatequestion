1be8d256ce69ef9c0366b54d2358213f77f6ade6
Andy Hardy <aohardy@gmail.com>
2021-11-06 17:02:38 -0600

    adding basic screens to app

---
+++ app/javascript/components/screens/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Text = styled.h1`
+    font-size: 24px;
+    font-weight: 800;
+    line-height: 32px;
+`;
+
+export default function About({}) {
+    return <Text>About</Text>
+}
